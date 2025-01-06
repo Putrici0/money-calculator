154655526fe501322610258394eed9578df79513
paull <paullopisparrilla@gmail.com>
2025-01-06 12:10:02 +0000

    Main created. App tested. Errors message working. Money-Calculator COMPLETED.

--- src/main/java/software/ulpgc/moneycalculator/Main.java
+++ src/main/java/software/ulpgc/moneycalculator/Main.java
@@ -1,4 +1,16 @@
 package software.ulpgc.moneycalculator;
 
+import software.ulpgc.moneycalculator.control.CurrencyController;
+import software.ulpgc.moneycalculator.model.CurrencyModel;
+import software.ulpgc.moneycalculator.service.CurrencyService;
+import software.ulpgc.moneycalculator.view.CurrencySwingView;
+
 public class Main {
+    public static void main(String[] args) {
+        CurrencyModel model = new CurrencyModel();
+        CurrencySwingView view = new CurrencySwingView();
+        CurrencyService service = new CurrencyService();
+        new CurrencyController(model, view, service);
+        view.setVisible(true);
+    }
 }
